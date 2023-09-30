5fe6fcd42794f8408a94942ce44338c8ae5eb7f5
VanaBlak <140449650+VanaBlak@users.noreply.github.com>
2023-09-29 19:53:05 -0600

    updated

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// Arrays
+let item_names = ["louis", "prada", "gucci"];
+let item_prices = [500, 600, 700];
+let in_stock = [true,false,true]
+
+// Replace Values
+let first_item_name = "balmain"
+item_names[0] = first_item_name
+console.log(item_names)
+
+item_prices[0] = 900
+console.log(item_prices)
+
+item_names.push("McQueen");
+console.log(item_names)
+
+item_prices.push(1000);
+console.log(item_prices)
+
+in_stock.push(false);
+console.log(in_stock)
